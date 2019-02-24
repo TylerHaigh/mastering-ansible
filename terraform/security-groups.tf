1205039ae85afb2437544e279673abccf5feae15
Tyler Haigh <tylerhaigh1994@me.com>
2019-02-24 15:15:54 +1100

    Proxy from load balancer to web server

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -41,9 +41,41 @@ resource "aws_security_group" "ansible_security_group" {
   }
 
   ingress {
+    description = "HTTP Alternate from Home IP"
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = [ "${chomp(data.http.myip.body)}/32" ]
+  }
+
+  ingress {
     description = "SSH from Internal IPs"
     from_port = 22
     to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [ "${aws_vpc.ansible_vpc.cidr_block}" ]
+  }
+
+  ingress {
+    description = "HTTP from Internal IPs"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [ "${aws_vpc.ansible_vpc.cidr_block}" ]
+  }
+
+  ingress {
+    description = "HTTPS from Internal IPs"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = [ "${aws_vpc.ansible_vpc.cidr_block}" ]
+  }
+
+  ingress {
+    description = "HTTP Alternate from Internal IPs"
+    from_port = 8080
+    to_port = 8080
     protocol = "tcp"
     cidr_blocks = [ "${aws_vpc.ansible_vpc.cidr_block}" ]
   }
